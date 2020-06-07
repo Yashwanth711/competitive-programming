20bc3bcd1a2e81edffd1b699c8abb12a0460b092
Yashwanth711 <47928682+Yashwanth711@users.noreply.github.com>
2020-06-07 17:41:41 +0530

    Add files via upload

---
+++ coinchange2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int change(int n, vector<int>& S) {
+        
+        int table[n+1]; 
+        
+        int m=S.size();
+  
+    // Initialize all table values as 0 
+    memset(table, 0, sizeof(table)); 
+  
+    // Base case (If given value is 0) 
+    table[0] = 1; 
+  
+    // Pick all coins one by one and update the table[] values 
+    // after the index greater than or equal to the value of the 
+    // picked coin 
+    for(int i=0; i<m; i++) 
+        for(int j=S[i]; j<=n; j++) 
+            table[j] += table[j-S[i]]; 
+  
+    return table[n]; 
+        
+    }
+};
